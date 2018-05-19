34a00d2c33f013cc96ba52673adc12a1fb7a7cfe
Kesireddy, Santosh (sk942g) <sk942g@att.com>
2018-05-19 15:01:08 -0700

    Created a simple angular app and bounded to the index.html

---
+++ app.js
@@ -0,0 +1,9 @@
+//module is a function on the angular object and myApp is the peace which connects the model and view togeather.   
+var myApp = angular.module("myApp",[]);
+//everything we add now will be added to this variable
+
+//first we are going to declare a controller 
+
+myApp.controller('mainController',function(){
+    //basically this is model(data) that the view(html) needs and the angular js binds them togeather  
+});//place to put the code associated with this controller
