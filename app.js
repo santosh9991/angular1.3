d09d0b285006423b773fe5281993620b6fb18ef7
santosh9991 <santu1santosh@gmail.com>
2018-05-19 22:48:55 -0700

    routing is implemented using ngRoute module and understood.

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
 //module is a function on the angular object and myApp is the peace which connects the model and view togeather.   
-var myApp = angular.module("myApp",['ngMessages']);//ngMessages[name of the module] is a modules that MyApp is dependent on. Once module is available, everything in the module is also available to us
+var myApp = angular.module("myApp",['ngRoute']);//ngMessages[name of the module] is a modules that MyApp is dependent on. Once module is available, everything in the module is also available to us
 //everything we add now will be added to this variable
 
 //first we are going to declare a controller 
 
+myApp.config(function ($routeProvider){
+    $routeProvider
+    .when('/',{
+        templateUrl:'/pages/main.html',
+        controller: 'mainController'
+    })
+    .when('/second',{
+        templateUrl:'/pages/second.html',
+        controller: 'secondController'
+    })
+})
 
-
-myApp.controller('mainController',["$scope","$filter","$timeout",function($scope,$filter,$timeout){//$scope is an object and $ is a part of varible name. All angular js services start with a $ sign. 
+myApp.controller('mainController',["$scope",function($scope){
     
-    $scope.name = "santosh";
-    //timeout is a angularjs service equivalent to settimeout in javascript
-    $timeout(function(){
-        $scope.name = "Krishna"
-    },3000) 
-    $scope.userName = '';
-    $scope.lowerCaseUserName = function(){
-        return $filter('lowercase')($scope.userName)
-    };
-}]);//place to put the code associated with this controller
+    $scope.name = "Main";
+}]);
+
+myApp.controller('secondController',["$scope",function($scope){
+    
+    $scope.name = "Second";
+}]);
