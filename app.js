b393e1c075da8b20937c1b1fcf7052f8b19c7d9d
santosh9991 <santu1santosh@gmail.com>
2018-05-19 17:14:30 -0700

    scope and interpolation , use of  to dynamically update the string is included and understood the concept behind it

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //module is a function on the angular object and myApp is the peace which connects the model and view togeather.   
-var myApp = angular.module("myApp",[]);
+var myApp = angular.module("myApp",['ngMessages']);//ngMessages[name of the module] is a modules that MyApp is dependent on. Once module is available, everything in the module is also available to us
 //everything we add now will be added to this variable
 
 //first we are going to declare a controller 
@@ -7,12 +7,15 @@ var myApp = angular.module("myApp",[]);
 
 
 myApp.controller('mainController',function($scope){//$scope is an object and $ is a part of varible name. All angular js services start with a $ sign. 
-    //adding properties to the $scope 
+    
     $scope.name = "santosh";
-    $scope.occupation= "developer";
-    console.log($scope);
-    // now you can use this values inside the index.html where this is used as a controller. 
-    //$scope is the one which establises the pipe between the model and view
-    //when you console it out you can see an object in the browser console, this is because it is an angular js object that it injects it into the function.
-    //basically this is model(data) that the view(html) needs and the angular js binds them togeather  
+    //timeout is a angularjs service equivalent to settimeout in javascript
+    $timeout(function(){
+        $scope.name = "Krishna"
+    },3000)
+    
+    
+    
+    
+
 });//place to put the code associated with this controller
